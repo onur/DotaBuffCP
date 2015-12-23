376c2e4fec20974375b9adfed5b7e02f89b6f880
aligo Kang <aligo_x@163.com>
2015-12-23 12:41:07 +0800

    add win rate to heroes list

--- dotabuffcp.js
+++ dotabuffcp.js
@@ -342,6 +342,7 @@ var MainView = Backbone.View.extend ({
       $('#' + div).append (_.template (template, {
                                      hero_bg: heroes_bg[advantage[1]],
                                      hero_name: heroes[advantage[1]],
+                                     win_rate: heroes_wr[advantage[1]],
                                      advantage: advantage[0].toFixed (2) * -1
                                                  }));
     });
